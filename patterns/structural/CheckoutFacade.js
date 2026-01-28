e2c16ae7c01937a1cd5ab8e0f194c91c5f00701d
TaiAu <auduongtai27@gmail.com>
2026-01-28 15:55:49 +0700

    3B: Facade

--- patterns/structural/CheckoutFacade.js
+++ patterns/structural/CheckoutFacade.js
@@ -17,6 +17,20 @@ class CheckoutFacade {
         // 2. If they are, process the payment using `paymentService.processPayment()`.
         // 3. If payment is successful, arrange shipping using `shippingService.arrangeShipping()`.
         // 4. Log the result of each step. If a step fails, log it and stop.
+        if (!this.inventoryService.checkStock(orderDetails.productIds)) {
+            console.log("Order failed: Product is out of stock.");
+            return
+        }
+
+        if (!this.paymentService.processPayment(orderDetails.userId,orderDetails.amount)) {
+            console.log("Order failed: Payment was declined.");
+            return; 
+        }
+
+        if (!this.shippingService.arrangeShipping(orderDetails.userId,orderDetails.shippingInfo)) {
+            console.log("Order failed: Could not arrange shipping (Invalid address or service unavailable).");
+            return;
+        }
     }
 }
 
